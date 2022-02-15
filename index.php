69b760de85a7976711a4a3a03306c50b77497944
tiomhs <88199655+tiomhs@users.noreply.github.com>
2022-02-15 08:02:45 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pilihan</title>
+</head>
+<body>
+
+    <h1>Lanjutkan sebagai?</h1>
+    <p><a href="petugas/index.php">Petugas</a> || <a href="siswa/index.php">Siswa</a></p>
+    
+</body>
+</html>
